87f7011e102cd7ede9b238c893b21a87785f27aa
Changfeng <louchangfeng@outlook.com>
2022-08-31 19:41:31 +0800

    feat(write): implement sys_write based on sys_call and rewrite println macro

---
+++ os/src/console.rs
@@ -0,0 +1,29 @@
+use crate::sys::sys_write;
+use core::fmt::Write;
+
+struct Stdout;
+
+impl Write for Stdout {
+    fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        sys_write(1, s.as_bytes());
+        Ok(())
+    }
+}
+
+pub fn print(args: core::fmt::Arguments) {
+    Stdout.write_fmt(args).unwrap();
+}
+
+#[macro_export]
+macro_rules! print {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!($fmt $(, $($arg)+)?));
+    }
+}
+
+#[macro_export]
+macro_rules! println {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
+    }
+}
